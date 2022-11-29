b0a198cbdab021843d9ee0aa3a881330c7e1d06a
Jebumon <jebumon@gmail.com>
2022-11-29 23:49:25 +0000

    Add project files.

---
+++ JBU-Cinemas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JBU_Cinemas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
