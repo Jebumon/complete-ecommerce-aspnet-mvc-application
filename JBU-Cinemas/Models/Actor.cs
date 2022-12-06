966292aa6dd11c342da97b673a693eb4348e777e
Jebumon <jebumon@gmail.com>
2022-12-06 01:34:09 +0000

    Solved : Actors - Index.cshtml alignment problems solved.

--- JBU-Cinemas/Models/Actor.cs
+++ JBU-Cinemas/Models/Actor.cs
@@ -6,7 +6,7 @@ namespace JBU_Cinemas.Models
     {
         [Key]
         public int ActorId { get; set; }
-        [Display(Name = "Profile Picture URL")]
+        [Display(Name = "Profile Picture")]
         public string ProfilePictureURL { get; set; }
         [Display(Name = "Full Name")]
         public string FullName { get; set; }
